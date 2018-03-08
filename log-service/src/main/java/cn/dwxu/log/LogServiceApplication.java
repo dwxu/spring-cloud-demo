43fdac77111a105bac14f51517eb30497608d59a
dongwuxu <dongwuxu@infinite.net.cn>
2018-03-08 17:53:36 +0800

    注册中心、服务应用、API网关

---
+++ log-service/src/main/java/cn/dwxu/log/LogServiceApplication.java
@@ -0,0 +1,15 @@
+package cn.dwxu.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@ComponentScan({"cn.dwxu"})
+public class LogServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LogServiceApplication.class);
+    }
+}
